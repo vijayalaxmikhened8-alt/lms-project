3ad092fa2a72068d5cbcd18ce9cd756e2b9dcb8a
vijayalaxmikhened8-alt <vijayalaxmikhened8@gmail.com>
2026-05-19 14:08:38 +0530

    Fix backend crashing on Render

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const dbPath = path.resolve(__dirname, 'lms.sqlite');
 
